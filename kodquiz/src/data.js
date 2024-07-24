7d4c84018a4cd84d04df51ab3ce3984d6071b964
Sreenijachowdary <sreenijachowdary42@gmail.com>
2024-07-24 16:35:28 +0530

    3rd commit

---
+++ kodquiz/src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+    {
+        question : '5 + 10',
+        option1 : '5',
+        option2 : '10',
+        option3 : '15',
+        option4 : '25',
+        ans : '3'
+    },
+    {
+        question : '2 * 20',
+        option1 : '20',
+        option2 : '40',
+        option3 : '60',
+        option4 : '80',
+        ans :'2'
+    },
+    {
+        question : '3 + 89',
+        option1 : '3',
+        option2 : '89',
+        option3 : '92',
+        option4 : '25',
+        ans :'3'
+    },
+    {
+        question : '6 / 3',
+        option1 : '3',
+        option2 : '6',
+        option3 : '2',
+        option4 : '1',
+        ans :'3'
+    },
+    {
+            question : '5 + 25',
+            option1 : '43',
+            option2 : '60',
+            option3 : '30',
+            option4 : '25',
+            ans :'3'
+    
+    },
+
+
+]
